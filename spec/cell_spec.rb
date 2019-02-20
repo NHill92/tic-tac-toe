9b5dccc3931b24a8e18dd7ef79f9a1cb444a29c7
Nate <nathanielhill.92@gmail.com>
2019-02-19 20:05:15 -0600

    Add player class and testing files

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -3,8 +3,8 @@ require "spec_helper"
 module TicTacToe
 
     RSpec.describe Cell do
-        
         context "#initialize" do
+            
             it "is initialized with a value of '' by default" do
                 cell = Cell.new
                 expect(cell.value).to eq ''
